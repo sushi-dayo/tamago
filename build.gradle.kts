0319817932c9767bcc6253865adf15fe3d70abd5
sya-ri <sya79lua@gmail.com>
2022-01-16 10:18:33 +0900

    chore: versions プラグインを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     kotlin("jvm") version "1.6.10"
     application
+    id("com.github.ben-manes.versions") version "0.41.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jmailen.kotlinter") version "3.8.0"
 }
